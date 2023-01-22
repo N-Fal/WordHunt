8822d95a3ea81672436c7f0952d3d4dc0afa0e5d
N-Fal <sidekick2005@gmail.com>
2023-01-22 02:17:29 -0800

    Tester file to test guesses

---
+++ src/Background/GuessTester.java
@@ -0,0 +1,26 @@
+package Background;
+
+import BoardGeneration.Board;
+import Solving.WordHunter;
+
+import java.util.Scanner;
+
+public class GuessTester
+{
+    public static void main(String[] args)
+    {
+        Board b = new Board(4, 4);
+        WordHunter hunter = new WordHunter(b);
+
+        Scanner kb = new Scanner(System.in);
+        String word = "";
+        do
+        {
+            System.out.println(b);
+            System.out.print("Enter a String: ");
+
+            word = kb.nextLine();
+            System.out.println(hunter.onBoard(word) ? word + " is present on the board." : word + " is not present on the board.");
+        } while (!word.equals("."));
+    }
+}

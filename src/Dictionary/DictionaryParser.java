706b33abbad6b58b621f2cc90b4a78594afb3986
N-Fal <sidekick2005@gmail.com>
2023-01-25 12:23:35 -0800

    Updated the dictionary to exempt 2 and 1 letter words

---
+++ src/Dictionary/DictionaryParser.java
@@ -0,0 +1,26 @@
+package Dictionary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DictionaryParser
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner dictReader = new Scanner(new File("src/Dictionary/dictionary.txt"));
+        FileWriter newDictWriter = new FileWriter("src/Dictionary/dictionaryNew.txt");
+
+        while(dictReader.hasNext())
+        {
+            String word = dictReader.nextLine();
+            if (word.length() >= 3)
+            {
+                newDictWriter.write(word + "\n");
+            }
+        }
+        newDictWriter.close();
+    }
+}

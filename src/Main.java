41c7bdff0912074a3e6894a2e7c1c35e23c6cd2d
N-Fal <sidekick2005@gmail.com>
2023-01-30 13:48:04 -0800

    Changed to match gui code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 // WORKING VERSION - This is the only branch that should be receiving pushes at the moment.
 
+import GUI.Frame;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        Frame f = new Frame();
     }
 }

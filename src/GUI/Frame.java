0cee163b9c2d5664b7895ca68efcc2c9af64037c
N-Fal <sidekick2005@gmail.com>
2023-02-02 13:43:47 -0800

    Added support for OptionsPanel.

--- src/GUI/Frame.java
+++ src/GUI/Frame.java
@@ -4,20 +4,31 @@ import BoardGeneration.Board;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
 public class Frame extends JFrame
 {
     private final int titleBarSize;
-    private JPanel mainPanel;
+    private GridLayout layout;
+
+    private BoardPanel boardPanel;
+    private OptionsPanel optionsPanel;
 
     public Frame()
     {
         this.setSize(720, 480);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Word Hunt");
-        this.add(new BoardPanel(new Board(4, 4), this));
+
+        layout = new GridLayout(1, 2);
+        this.setLayout(layout);
+        boardPanel = new BoardPanel(new Board(4, 4), this);
+        optionsPanel = new OptionsPanel(this, boardPanel);
+
+        this.add(boardPanel);
+        this.add(optionsPanel);
+
+
+
         this.setFocusable(true);
         this.setVisible(true);
 
@@ -27,5 +38,30 @@ public class Frame extends JFrame
     public int getTitleBarSize()
     {
         return titleBarSize;
+    }
+
+    public OptionsPanel getOptionsPanel()
+    {
+        return optionsPanel;
+    }
+
+    @Override
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+
+        if (getWidth() > getHeight())
+        {
+            layout.setRows(1);
+            layout.setColumns(2);
+        }
+        else
+        {
+            layout.setRows(2);
+            layout.setColumns(1);
+        }
+
+        // ALMOST makes everything perfect but breaks it instead
+        // this.pack();
     }
 }
